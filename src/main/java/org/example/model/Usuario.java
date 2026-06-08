1141b82d589fd9284ceb07c4934bb8e5098405ce
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-08 09:21:50 -0300

    criando classe de modelo

---
+++ src/main/java/org/example/model/Usuario.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data @AllArgsConstructor @ToString
+public class Usuario {
+
+    private String email;
+    private String senha;
+
+}
