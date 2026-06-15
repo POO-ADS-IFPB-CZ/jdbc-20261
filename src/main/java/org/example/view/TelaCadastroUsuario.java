6b41895a0932d4deef6417acc628f511721df85d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-15 09:06:30 -0300

    Validando formulário

--- src/main/java/org/example/view/TelaCadastroUsuario.java
+++ src/main/java/org/example/view/TelaCadastroUsuario.java
@@ -1,6 +1,13 @@
 package org.example.view;
 
+import org.example.dao.UsuarioDaoJdbc;
+import org.example.model.Usuario;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class TelaCadastroUsuario extends JDialog {
     private JPanel contentPane;
@@ -9,10 +16,48 @@ public class TelaCadastroUsuario extends JDialog {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JPasswordField passwordField2;
+    private UsuarioDaoJdbc usuarioDao;
 
     public TelaCadastroUsuario() {
+        usuarioDao = new UsuarioDaoJdbc();
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        buttonCancel.addActionListener(e->{
+            dispose();
+        });
+        buttonOK.addActionListener(e ->{
+            if(validarFormulario()){
+
+            }
+        });
     }
+
+    private boolean validarFormulario(){
+        if(textField1.getText().equals("") ||
+                passwordField1.getPassword().length==0 ||
+                passwordField2.getPassword().length==0
+        ){
+            exibirMensagemErro("Preencha todos os campos");
+            return false;
+        }
+        if(!textField1.getText().contains("@")){
+            exibirMensagemErro("Informe um e-mail válido");
+            return false;
+        }
+        if(!Arrays.equals(passwordField1.getPassword(),
+                passwordField2.getPassword())){
+            exibirMensagemErro("As senhas não conferem");
+            return false;
+        }
+        return true;
+    }
+
+    private void exibirMensagemErro(String mensagem){
+        JOptionPane.showMessageDialog(null,
+                mensagem,
+                "Mensagem de erro",
+                JOptionPane.ERROR_MESSAGE);
+    }
+
 }
