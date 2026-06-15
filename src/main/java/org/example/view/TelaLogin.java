c7ac8d1eb3bff8a9f33507351165e3c24e9ad6ac
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-15 08:45:54 -0300

    Criando função para tela de login

--- src/main/java/org/example/view/TelaLogin.java
+++ src/main/java/org/example/view/TelaLogin.java
@@ -1,9 +1,14 @@
 package org.example.view;
 
+import org.example.dao.UsuarioDaoJdbc;
+import org.example.model.Usuario;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class TelaLogin extends JFrame {
     private JPanel contentPane;
@@ -12,8 +17,10 @@ public class TelaLogin extends JFrame {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JLabel label1;
+    private UsuarioDaoJdbc usuarioDao;
 
     public TelaLogin() {
+        usuarioDao = new UsuarioDaoJdbc();
         setContentPane(contentPane);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getRootPane().setDefaultButton(buttonOK);
@@ -21,6 +28,37 @@ public class TelaLogin extends JFrame {
             TelaCadastroUsuario cadastro = new TelaCadastroUsuario();
             cadastro.pack();
             cadastro.setVisible(true);
+        });
+        buttonOK.addActionListener(e->{
+            Usuario usuario = null;
+            try{
+                usuario = usuarioDao.getUsuariobyEmail(
+                        textField1.getText());
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Falha na conexão com o banco",
+                        "Mensagem de Erro",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+            if(usuario == null){
+                JOptionPane.showMessageDialog(null,
+                        "Usuário não encontrado",
+                        "Mensagem de erro",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+            if(usuario != null){
+                String senhaDigitada = new String(
+                        passwordField1.getPassword());
+                if(!senhaDigitada.equals(usuario.getSenha())){
+                    JOptionPane.showMessageDialog(null,
+                            "Senha incorreta",
+                            "Mensagem de erro",
+                            JOptionPane.ERROR_MESSAGE);
+                }else{
+                    JOptionPane.showMessageDialog(null,
+                            "Bem vindo");
+                }
+            }
         });
     }
 
