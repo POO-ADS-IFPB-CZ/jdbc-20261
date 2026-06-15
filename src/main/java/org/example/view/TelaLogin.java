f5f634704003bf7d728b2daf058c19c722774cf2
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-15 08:02:14 -0300

    criando form na tela de login

--- src/main/java/org/example/view/TelaLogin.java
+++ src/main/java/org/example/view/TelaLogin.java
@@ -1,11 +1,15 @@
 package org.example.view;
 
 import javax.swing.*;
+import java.io.File;
 
 public class TelaLogin extends JFrame {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JLabel label1;
 
     public TelaLogin() {
         setContentPane(contentPane);
@@ -16,5 +20,12 @@ public class TelaLogin extends JFrame {
         TelaLogin dialog = new TelaLogin();
         dialog.pack();
         dialog.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        label1 = new JLabel();
+        ImageIcon icon = new ImageIcon(
+                "img/login.png");
+        label1.setIcon(icon);
     }
 }
